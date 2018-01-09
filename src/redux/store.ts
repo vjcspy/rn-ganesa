1e87bf1b25022d9f62178880b7ce05e56e293855
khoild <khoild@smartosc.com>
2018-01-09 14:18:00 +0700

    - pass store$ as stream

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -41,29 +41,3 @@ export function replaceModuleReducer(key, reducer) {
 
 export default store;
 
-
-// Streaming Redux state as an Observable with RxJS
-
-function getState$(store) {
-    return new Observable(observer => {
-        // emit the current state as first value:
-        observer.next(store.getState());
-        
-        const unsubscribe = store.subscribe(() => {
-            // emit on every new state changes
-            observer.next(store.getState());
-        });
-        
-        // let's return the function that will be called
-        // when the Observable is unsubscribed
-        return unsubscribe;
-    });
-}
-
-let storeStream       = getState$(store).share();
-storeStream["select"] = (key: string) => {
-    return storeStream.map((state) => state[key]).distinctUntilChanged();
-};
-
-
-export const store$ = storeStream;
