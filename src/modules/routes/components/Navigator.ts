78318a9c17937107a8895f0d93f647e59ced0874
khoild <khoild@smartosc.com>
2018-01-25 16:38:47 +0700

    - minor clean up

--- src/modules/routes/components/Navigator.ts
+++ src/modules/routes/components/Navigator.ts
@@ -5,7 +5,6 @@ import {PosOutletViewContainer} from "../../pos/pages/outlet-register/Outlet";
 const AppNavigator = StackNavigator({
     login: {screen: AuthLoginContainer},
     outlet: {screen: PosOutletViewContainer}
-    //InfiniteColorStack: {screen: ColorViewContainer}
 }, {
     headerMode: "none"
 });
